d620f01206ee39e3ccf5ba17f1bbef84825526b5
Kimberly Bordon and Kyle Ebersole <kyle@>
2017-09-18 13:52:11 -0700

    Add function to sort and uppercase list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
   $("#groceries").submit(function(event){
     var groceries = ["one", "two", "three", "four"];
+    var newGroceries = [];
 
-    groceries.forEach(function(grocery){
+    groceries.forEach(function(grocery) {
       var groceryInput = $("input#" + grocery).val();
-      $(".list").append("<li>" + groceryInput + "</li>");
+      newGroceries.push(groceryInput);
     });
 
+    newGroceries.sort();
+
+    var upperGroceries = newGroceries.map(function(grocery){
+      return grocery.toUpperCase();
+    });
+
+
+    // groceries.forEach(function(grocery){
+    //   var groceryInput = $("input#" + grocery).val();
+    //   $(".list").append("<li>" + groceryInput + "</li>");
+    // });
     event.preventDefault();
   });
-
 });
