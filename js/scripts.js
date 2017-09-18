7042ac5e6c4089a302cca0de494abefaa924e900
Kimberly Bordon and Kyle Ebersole <kimberly@>
2017-09-18 12:01:14 -0700

    Add function to output initial list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+  $("#groceries").submit(function(event){
+    var groceries = ["one", "two", "three", "four"];
 
+    groceries.forEach(function(grocery){
+      var groceryInput = $("input#" + grocery).val();
+      $(".list").append("<li>" + groceryInput + "</li>");
+    });
+
+    event.preventDefault();
+  });
 
 });
